7dccfa87dd89a4f6f10b4baebfc0b157dd83c64b
StefaansBothma <stefaansbothma@hotmail.com>
2014-08-20 06:46:27 +0200

    Initial Commit

---
+++ RSSReader/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using RSSReader.Models;
+
+namespace RSSReader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public  ActionResult Feed()
+        {
+            var items = new List<SyndicationItem>();
+
+            var feed = SyndicationFeed.Load(XmlReader.Create("http://feeds.news24.com/articles/news24/TopStories/rss"));
+            ViewBag.feedTitle = "No Title";
+
+            if (feed != null)
+            {
+                ViewBag.feedTitle = feed.Title.Text;
+                items.AddRange(
+                    feed.Items.Select(item => item.Title.Text != null && item.Summary.Text != null && item.Links[0] != null ? new SyndicationItem(item.Title.Text, item.Summary.Text, item.Links[0].Uri)
+                        { PublishDate = item.PublishDate } : null));
+            }
+
+            ViewBag.Feeds = items;
+            return View();
+        }
+
+        public ActionResult LogViewedFeed(string feedTitle)
+        {
+            bool read;
+            return Feed();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
